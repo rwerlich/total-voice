2f2924410fd94ea6173b3f39ae4add54823e6b3b
Rodrigo Werlich <rodrigowerlich@clincard.com.br>
2018-11-23 14:47:50 -0200

    removido token

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ require 'vendor/autoload.php';
 
 use App\ApiTotalVoice;
 
-$api = new ApiTotalVoice('76b2bd1f409cdc2e0fb46c2803f00feb');
-$api->sendSms('48996190961','SMS enviado via Socket');
+$api = new ApiTotalVoice('xxxxxxxx'); //insira aqui seu token
+$api->sendSms('48999999999','SMS enviado via Socket');
